7ce03f05869877eb255aeeab3fe20ac10412a241
Daisyy2 <dmunoz7@students.kennesaw.edu>
2024-04-21 22:34:28 -0400

    Created scrollbar

--- to do list.py
+++ to do list.py
@@ -24,11 +24,21 @@ my_list = Listbox(my_frame, font=my_font,
                  selectbackground="#a6a6a6",
                  activestyle="none")
 
-my_list.pack()
+my_list.pack(side=LEFT, fill=BOTH)
 
 #Create dummy list
 stuff = ["Walk the dog", "Buy groceries", "Take a nap", "Learn Tkinter"]
 #Add dummy list to list box
 for item in stuff:
     my_list.insert(END, item)
+
+#Create scrollbar
+my_scrollbar = Scrollbar(my_frame)
+my_scrollbar.pack(side=RIGHT, fill=BOTH)
+
+#Add scrollbar
+my_list.config(yscrollcommand=my_scrollbar.set)
+my_scrollbar.config(command=my_list.yview)
+
+
 root.mainloop()
