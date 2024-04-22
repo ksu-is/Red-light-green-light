c6767dc4640b402f4886c946ba653b2e344dea9d
Daisyy2 <dmunoz7@students.kennesaw.edu>
2024-04-21 22:11:54 -0400

    Create frame and list box
    added element to list box

--- to do list.py
+++ to do list.py
@@ -9,4 +9,15 @@ root.geometry("500x500")
 #Define our Font
 my_font = Font(family="Baskerville Old Face", size=30, weight="bold")
 
+#Create Frame
+my_frame = Frame(root)
+my_frame.pack(pady=10)
+
+#Create listbox
+my_list = Listbox(my_frame, font=my_font, width=25, height=5, bg="SystemButtonFace", bd=0, fg="#464646")
+
+my_list.pack()
+
+stuff = ["Walk the dog", "Buy groceries", "Take a nap", "Learn Tkinter"]
+
 root.mainloop()
