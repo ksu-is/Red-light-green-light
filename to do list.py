08cac8697a71c138023f3240b328ff8ad2a42a0d
Daisyy2 <dmunoz7@students.kennesaw.edu>
2024-04-21 22:04:24 -0400

    Editing font

--- to do list.py
+++ to do list.py
@@ -1,10 +1,12 @@
 from tkinter import *
+from tkinter.font import Font
 
 root = Tk()
 root.title('Red light, Green light - Task Manager!')
 root.iconbitmap('c:/gui/Red light, Green light.ico')
 root.geometry("500x500")
 
-
+#Define our Font
+my_font = Font(family="Baskerville Old Face", size=30, weight="bold")
 
 root.mainloop()
