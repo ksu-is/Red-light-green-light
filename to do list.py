c3b0724f87662e5eb061202e777c1d9803171af5
Daisyy2 <dmunoz7@students.kennesaw.edu>
2024-04-21 21:58:24 -0400

    Starter code

--- to do list.py
+++ to do list.py
@@ -2,3 +2,9 @@ from tkinter import *
 
 root = Tk()
 root.title('Red light, Green light - Task Manager!')
+root.iconbitmap('c:/gui/Red light, Green light.ico')
+root.geometry("500x500")
+
+
+
+root.mainloop()
