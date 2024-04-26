49111351bdb87dfa1864aaa470f2ca8d96169376
Daisyy2 <dmunoz7@students.kennesaw.edu>
2024-04-26 16:56:15 -0400

    add spaces to comments

--- to do list.py
+++ to do list.py
@@ -52,14 +52,14 @@ def add_item():
     my_list.insert(END, my_entry.get())
     my_entry.delete(0, END)
 def cross_off_item():
-    #Cross off item
+    # Cross off item
     my_list.itemconfig(my_list.curselection(), fg="#A3A3A3")
-    #Get rid of selection bar entry
+    # Get rid of selection bar entry
     my_list.selection_clear(0,END)
 def uncross_item():
-    #Cross off item
+    # Cross off item
     my_list.itemconfig(my_list.curselection(), fg="#000000")
-    #Get rid of selection bar entry
+    # Get rid of selection bar entry
     my_list.selection_clear(0,END)
 def delete_crossed():
     count = 0
@@ -69,19 +69,19 @@ def delete_crossed():
         else:
              count += 1
 def red_item():
-    #Change color to red
+    # Change color to red
     my_list.itemconfig(my_list.curselection(), fg="#FF0000")
-    #Get rid of selection bar entry
+    # Get rid of selection bar entry
     my_list.selection_clear(0,END)
 def orange_item():
-    #Change color to orange
+    # Change color to orange
     my_list.itemconfig(my_list.curselection(), fg="#FFA500")
-    #Get rid of selection bar entry
+    # Get rid of selection bar entry
     my_list.selection_clear(0,END)
 def green_item():
-    #Change color to green
+    # Change color to green
     my_list.itemconfig(my_list.curselection(), fg="#00FF00")
-    #Get rid of selection bar entry
+    # Get rid of selection bar entry
     my_list.selection_clear(0,END)
 
 # Menu Functions
@@ -109,7 +109,7 @@ def save_list():
         # Grab all stuff form list
         stuff = my_list.get(0, END)
 
-        #Open file
+        # Open file
         output_file = open(file_name, 'wb')
 
         # Actaully add stuff to the file
