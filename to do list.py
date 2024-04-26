55e00e66bbb48886090321d9f0b6b95e69fc0767
Daisyy2 <dmunoz7@students.kennesaw.edu>
2024-04-26 15:14:19 -0400

    Adjust dummy list

--- to do list.py
+++ to do list.py
@@ -27,7 +27,11 @@ my_list = Listbox(my_frame, font=my_font,
 my_list.pack(side=LEFT, fill=BOTH)
 
 #Create dummy list
-stuff = ["Walk the dog", "Buy groceries", "Take a nap", "Learn Tkinter"]
+stuff = ["Walk the dog",
+          "Buy groceries",
+          "Get an oil change",
+          "Go to the gym"]
+
 #Add dummy list to list box
 for item in stuff:
     my_list.insert(END, item)
