81a57f852a9c3e2ef2c2453eda5cf847e7694e78
Daisyy2 <dmunoz7@students.kennesaw.edu>
2024-04-26 15:52:14 -0400

    Add spaces

--- to do list.py
+++ to do list.py
@@ -6,14 +6,14 @@ root.title('Red light, Green light - Task Manager!')
 root.iconbitmap('c:/gui/Red light, Green light.ico')
 root.geometry("550x600")
 
-#Define our Font
+# Define our Font
 my_font = Font(family="Brush Script MT", size=30)
 
-#Create Frame
+# Create Frame
 my_frame = Frame(root)
 my_frame.pack(pady=10)
 
-#Create listbox
+# Create listbox
 my_list = Listbox(my_frame, font=my_font,
                  width=30, 
                  height=8,
@@ -26,34 +26,34 @@ my_list = Listbox(my_frame, font=my_font,
 
 my_list.pack(side=LEFT, fill=BOTH)
 
-#Create dummy list
+# Create dummy list
 stuff = ["Walk the dog",
           "Buy groceries",
           "Get an oil change",
           "Go to the gym"]
 
-#Add dummy list to list box
+# Add dummy list to list box
 for item in stuff:
     my_list.insert(END, item)
 
-#Create scrollbar
+# Create scrollbar
 my_scrollbar = Scrollbar(my_frame)
 my_scrollbar.pack(side=RIGHT, fill=BOTH)
 
-#Add scrollbar
+# Add scrollbar
 my_list.config(yscrollcommand=my_scrollbar.set)
 my_scrollbar.config(command=my_list.yview)
 
-#create entry box to add items to the list
+# Create entry box to add items to the list
 my_entry = Entry(root, font=("Helvetica", 24), width= 30)
 my_entry.pack(pady= 20)
 
-#Create a button frame
+# Create a button frame
 button_frame = Frame(root)
 button_frame.pack(pady= 20)
 
 
-#FUNCTIONS
+# FUNCTIONS
 def delete_item():
     my_list.delete(ANCHOR)
 def add_item():
@@ -77,11 +77,11 @@ def delete_crossed():
         else:
              count += 1
 
-#Create Menu
+# Create Menu
 my_menu = Menu(root)
 root.config (menu = my_menu)
 
-#Add items to the menu
+# Add items to the menu
 file_menu = Menu(my_menu, tearoff=False)
 my_menu.add_cascade(label="File", menu=file_menu)
 #Add dropdown items
@@ -91,14 +91,14 @@ file_menu.add_separator()
 file_menu.add_command(label="Clear List", command=clear_list)
 
 
-#Add buttons to frame
+# Add buttons to frame
 delete_button = Button(button_frame, text="Delete Item", command=delete_item)
 add_button = Button(button_frame, text="Add Item", command=add_item)
 cross_off_button = Button(button_frame, text="Cross Off Item", command=cross_off_item)
 uncross_button = Button(button_frame, text="Uncross Item", command=uncross_item)
 delete_crossed_button = Button(button_frame, text="Delete Crossed", command=delete_crossed)
 
-#Add measurements of buttons
+# Add measurements of buttons
 delete_button.grid(row=0, column=0)
 add_button.grid(row=0, column=1, padx=20)
 cross_off_button.grid(row=0, column=2)
